c533a1ab76c150ea4ffa3caa723522b0c7c1a8dd
Ahmed Safan <ahmed.safan@aurea.com>
2020-07-14 02:25:47 +0200

    Upgrading to 3.1

--- AuthorizeNET/AuthorizeNET/Utilities/LogFactory.cs
+++ AuthorizeNET/AuthorizeNET/Utilities/LogFactory.cs
@@ -2,14 +2,36 @@
 {
     using System;
     using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Logging.Debug;
 
     public static class LogFactory
     {
-        private static ILoggerFactory LoggerFactory => new LoggerFactory().AddDebug(LogLevel.Debug);
+        private static ILoggerFactory LoggerFactory { get; } = CreateLoggerFactory();
+
+        //TODO: Better to inject factory at app initialization
+        //private static ILoggerFactory LoggerFactory { get; set; }
+
+        //public static void Initialize(ILoggerFactory loggerFactory)
+        //{
+        //    LoggerFactory = loggerFactory;
+        //}
 
         public static ILogger getLog(Type classType)
         {
+            if (LoggerFactory == null)
+            {
+                throw new ArgumentNullException(
+                    nameof(LoggerFactory),
+                    $"Make sure you called LogFactory.Initialize() on application startup");
+            }
             return LoggerFactory.CreateLogger(classType.FullName);
+        }
+
+        private static LoggerFactory CreateLoggerFactory()
+        {
+            var factory = new LoggerFactory();
+            factory.AddProvider(new DebugLoggerProvider());
+            return factory;
         }
     }
 }
